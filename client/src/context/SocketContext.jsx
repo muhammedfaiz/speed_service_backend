56792316402f9b2f010f630f4e4c3630b545ab33
muhammedfaiz <faizmuhammed342@gmail.com>
2026-07-24 13:06:43 +0300

    socket fix

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketProvider = ({ children }) => {
     const {employee}=useSelector(store=>store.employee);
     useEffect(()=>{
         if(user || employee){
-            const socket = io('https://api.speedservice.store',{
+            const socket = io('https://speed-service.onrender.com',{
                 query:{
                     userId:user?.id,
                     employeeId:employee?.id,
